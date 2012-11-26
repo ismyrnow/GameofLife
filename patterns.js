064a81c856f4f6536222c129f3e8aa6b51064ace
Ishmael Smyrnow <ismyrnow@gmail.com>
2012-11-25 23:19:12 -0500

    Adding source code

---
+++ patterns.js
@@ -0,0 +1,40 @@
+(function() {
+	
+	window.app = {};
+
+	app.setPattern = function(pattern, grid) {
+
+		switch(pattern) {
+
+			case "toad":			
+				grid[1][1] = 1;
+				grid[2][1] = 1;
+				grid[3][1] = 1;
+				grid[0][2] = 1;
+				grid[1][2] = 1;
+				grid[2][2] = 1;
+				break;
+
+			case "flyer":			
+				grid[0][0] = 1;
+				grid[0][2] = 1;
+				grid[1][2] = 1;
+				grid[1][1] = 1;
+				grid[2][1] = 1;
+				break;
+
+			case "acorn":
+				grid[17][14] = 1;
+				grid[19][15] = 1;
+				grid[16][16] = 1;
+				grid[17][16] = 1;
+				grid[20][16] = 1;
+				grid[21][16] = 1;
+				grid[22][16] = 1;
+				break;
+
+		}
+
+	};
+
+})();
