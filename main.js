93914f3df9ff1cfd41119ece9a9c6671091d595d
Ishmael Smyrnow <ismyrnow@gmail.com>
2014-04-17 22:15:09 -0400

    Add jshint, switch to 2 space indent

--- main.js
+++ main.js
@@ -1,165 +1,167 @@
-$(function() {
+/* global $ */
+$(function () {
+  'use strict';
 
-	var ctx; // canvas 2d context
-	var grid; // 2d array of 1's and 0's (live and dead)
-	var bounds;
-	var cellSize = 14;
-	var gridSize = 25;
+  var ctx; // canvas 2d context
+  var grid; // 2d array of 1's and 0's (live and dead)
+  var bounds;
+  var cellSize = 14;
+  var gridSize = 25;
 
-	init();
+  init();
 
-	function init() {
-		// init canvas
-		var canvas = document.getElementById("canvas");
-		canvas.width = cellSize * gridSize + 2 * gridSize + 2;
-		canvas.height = cellSize * gridSize + 2 * gridSize + 2;
-		ctx = canvas.getContext("2d");
-		drawGrid();
+  function init() {
+    // init canvas
+    var canvas = document.getElementById('canvas');
+    canvas.width = cellSize * gridSize + 2 * gridSize + 2;
+    canvas.height = cellSize * gridSize + 2 * gridSize + 2;
+    ctx = canvas.getContext('2d');
+    drawGrid();
 
-		// init bounds
-		bounds = {
-			xMin: 0,
-			xMax: gridSize,
-			yMin: 0,
-			yMax: gridSize
-		};
+    // init bounds
+    bounds = {
+      xMin: 0,
+      xMax: gridSize,
+      yMin: 0,
+      yMax: gridSize
+    };
 
-		// init grid
-		grid = createGrid();
+    // init grid
+    grid = createGrid();
 
-		// init pattern
-		app.setPattern("acorn", grid);
+    // init pattern
+    window.app.setPattern('acorn', grid);
 
-		// start program
+    // start program
 
-		var id;
-		var count = 500;
-		id = setInterval(function() {
-			redraw();
-			tick();
-			if (count-- === 0) {
-				clearInterval(id);
-			}
-		}, 200);
-	}
+    var id;
+    var count = 500;
+    id = setInterval(function () {
+      redraw();
+      tick();
+      if (count-- === 0) {
+        clearInterval(id);
+      }
+    }, 200);
+  }
 
-	function createGrid() {
-		var grid = new Array(bounds.xMax);
+  function createGrid() {
+    var grid = new Array(bounds.xMax);
 
-		for (var x = bounds.xMin - 1; x <= bounds.xMax + 1; x++) {
-			grid[x] = new Array(bounds.yMax + 1);
-			for (var y = bounds.yMin - 1; y <= bounds.yMax + 1; y++) {
-				grid[x][y] = 0;
-			}
-		}
+    for (var x = bounds.xMin - 1; x <= bounds.xMax + 1; x++) {
+      grid[x] = new Array(bounds.yMax + 1);
+      for (var y = bounds.yMin - 1; y <= bounds.yMax + 1; y++) {
+        grid[x][y] = 0;
+      }
+    }
 
-		return grid;	
-	}
+    return grid;
+  }
 
-	function tick() {
-		var newGrid = createGrid();
-		var newBounds = cloneBounds();
+  function tick() {
+    var newGrid = createGrid();
+    var newBounds = cloneBounds();
 
-		forEachCell(function(x, y) {
-			var alive = checkIsAlive(x, y);
-			var neighbors = countLiveNeighbors(x, y);
+    forEachCell(function (x, y) {
+      var alive = checkIsAlive(x, y);
+      var neighbors = countLiveNeighbors(x, y);
 
-			if (alive && neighbors < 2) {
-				newGrid[x][y] = 0;
-			} else if (alive && (neighbors === 2 || neighbors === 3)) {
-				newGrid[x][y] = 1;
-				extendBounds(newBounds, x, y);
-			} else if (alive && neighbors > 3) {
-				newGrid[x][y] = 0;
-			} else if (!alive && neighbors === 3) {
-				newGrid[x][y] = 1;
-				extendBounds(newBounds, x, y);
-			}
-		});
+      if (alive && neighbors < 2) {
+        newGrid[x][y] = 0;
+      } else if (alive && (neighbors === 2 || neighbors === 3)) {
+        newGrid[x][y] = 1;
+        extendBounds(newBounds, x, y);
+      } else if (alive && neighbors > 3) {
+        newGrid[x][y] = 0;
+      } else if (!alive && neighbors === 3) {
+        newGrid[x][y] = 1;
+        extendBounds(newBounds, x, y);
+      }
+    });
 
-		grid = newGrid;
-		bounds = newBounds;
-	}
+    grid = newGrid;
+    bounds = newBounds;
+  }
 
-	function countLiveNeighbors(x, y) {
-		var n = 0;
+  function countLiveNeighbors(x, y) {
+    var n = 0;
 
-		for (var dx = -1; dx < 2; dx++) {
-			for (var dy = -1; dy < 2; dy++) {
-				if (grid[x + dx] && grid[x + dx][y + dy]) {
-					n += grid[x + dx][y + dy];
-				}
-			}
-		}
+    for (var dx = -1; dx < 2; dx++) {
+      for (var dy = -1; dy < 2; dy++) {
+        if (grid[x + dx] && grid[x + dx][y + dy]) {
+          n += grid[x + dx][y + dy];
+        }
+      }
+    }
 
-		if (grid[x] && grid[x][y]) {
-			n -= grid[x][y];
-		}
+    if (grid[x] && grid[x][y]) {
+      n -= grid[x][y];
+    }
 
-		return n;
-	}
+    return n;
+  }
 
-	function cloneBounds() {
-		var newBounds = {
-			xMin: bounds.xMin,
-			xMax: bounds.xMax,
-			yMin: bounds.yMin,
-			yMax: bounds.yMax
-		};
+  function cloneBounds() {
+    var newBounds = {
+      xMin: bounds.xMin,
+      xMax: bounds.xMax,
+      yMin: bounds.yMin,
+      yMax: bounds.yMax
+    };
 
-		return newBounds;
-	}
+    return newBounds;
+  }
 
-	function extendBounds(bounds, x, y) {
-		bounds.xMin = Math.min(bounds.xMin, x);
-		bounds.xMax = Math.max(bounds.xMax, x);
-		bounds.yMin = Math.min(bounds.yMin, y);
-		bounds.yMax = Math.max(bounds.yMax, y);
-	}
+  function extendBounds(bounds, x, y) {
+    bounds.xMin = Math.min(bounds.xMin, x);
+    bounds.xMax = Math.max(bounds.xMax, x);
+    bounds.yMin = Math.min(bounds.yMin, y);
+    bounds.yMax = Math.max(bounds.yMax, y);
+  }
 
-	function redraw() {
-		forEachCell(drawCell);
-	}
+  function redraw() {
+    forEachCell(drawCell);
+  }
 
-	function checkIsAlive(x, y) {
-		return grid[x] && grid[x][y] && grid[x][y] === 1;
-	}
+  function checkIsAlive(x, y) {
+    return grid[x] && grid[x][y] && grid[x][y] === 1;
+  }
 
-	function drawGrid() {		
-		var width = cellSize * gridSize + 2 * gridSize + 2;
-		var height = cellSize * gridSize + 2 * gridSize + 2;
+  function drawGrid() {
+    var width = cellSize * gridSize + 2 * gridSize + 2;
+    var height = cellSize * gridSize + 2 * gridSize + 2;
 
-		ctx.fillStyle = "rgb(200,200,200)";
-		for (var x = 0; x <= (width - 2) / cellSize; x++) {
-			// horizontal
-			ctx.fillRect(
-				0,
-				(x * cellSize) + (x * 2),
-				width,
-				2
-			);
-			// vertical
-			ctx.fillRect(
-				(x * cellSize) + (x * 2),
-				0,
-				2,
-				height
-			);
-		}
-	}
+    ctx.fillStyle = 'rgb(200,200,200)';
+    for (var x = 0; x <= (width - 2) / cellSize; x++) {
+      // horizontal
+      ctx.fillRect(
+        0,
+        (x * cellSize) + (x * 2),
+        width,
+        2
+      );
+      // vertical
+      ctx.fillRect(
+        (x * cellSize) + (x * 2),
+        0,
+        2,
+        height
+      );
+    }
+  }
 
-	function drawCell(x, y) {
-		var isAlive = checkIsAlive(x, y);
-		ctx.fillStyle = isAlive ? "rgb(0,0,0)" : "rgb(255,255,255)";
-		ctx.fillRect(x*cellSize + x*2 + 2, y*cellSize + y*2 + 2, cellSize, cellSize);
-	}
+  function drawCell(x, y) {
+    var isAlive = checkIsAlive(x, y);
+    ctx.fillStyle = isAlive ? 'rgb(0,0,0)' : 'rgb(255,255,255)';
+    ctx.fillRect(x * cellSize + x * 2 + 2, y * cellSize + y * 2 + 2, cellSize, cellSize);
+  }
 
-	function forEachCell(fn) {
-		for (var x = bounds.xMin - 1; x <= bounds.xMax + 1; x++) {
-			for (var y = bounds.yMin - 1; y <= bounds.yMax + 1; y++) {
-				fn(x, y);
-			}
-		}
-	}
+  function forEachCell(fn) {
+    for (var x = bounds.xMin - 1; x <= bounds.xMax + 1; x++) {
+      for (var y = bounds.yMin - 1; y <= bounds.yMax + 1; y++) {
+        fn(x, y);
+      }
+    }
+  }
 
 });
--- patterns.js
+++ patterns.js
@@ -1,40 +1,41 @@
-(function() {
-	
-	window.app = {};
+(function () {
+  'use strict';
 
-	app.setPattern = function(pattern, grid) {
+  window.app = {};
 
-		switch(pattern) {
+  window.app.setPattern = function (pattern, grid) {
 
-			case "toad":			
-				grid[1][1] = 1;
-				grid[2][1] = 1;
-				grid[3][1] = 1;
-				grid[0][2] = 1;
-				grid[1][2] = 1;
-				grid[2][2] = 1;
-				break;
+    switch (pattern) {
 
-			case "flyer":			
-				grid[0][0] = 1;
-				grid[0][2] = 1;
-				grid[1][2] = 1;
-				grid[1][1] = 1;
-				grid[2][1] = 1;
-				break;
+      case 'toad':
+        grid[1][1] = 1;
+        grid[2][1] = 1;
+        grid[3][1] = 1;
+        grid[0][2] = 1;
+        grid[1][2] = 1;
+        grid[2][2] = 1;
+        break;
 
-			case "acorn":
-				grid[17][14] = 1;
-				grid[19][15] = 1;
-				grid[16][16] = 1;
-				grid[17][16] = 1;
-				grid[20][16] = 1;
-				grid[21][16] = 1;
-				grid[22][16] = 1;
-				break;
+      case 'flyer':
+        grid[0][0] = 1;
+        grid[0][2] = 1;
+        grid[1][2] = 1;
+        grid[1][1] = 1;
+        grid[2][1] = 1;
+        break;
 
-		}
+      case 'acorn':
+        grid[17][14] = 1;
+        grid[19][15] = 1;
+        grid[16][16] = 1;
+        grid[17][16] = 1;
+        grid[20][16] = 1;
+        grid[21][16] = 1;
+        grid[22][16] = 1;
+        break;
 
-	};
+    }
+
+  };
 
 })();
